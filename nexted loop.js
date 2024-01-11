34bbfa840716b84aedea1d0c0a8c933382598fa7
Adesanya Abdullateef Adebowale <debo@abdullateet.dev>
2024-01-11 22:01:14 +0100

    Add files via upload

---
+++ nexted loop.js
@@ -0,0 +1,14 @@
+/*
+for (var year = 2023; year < 2025; year++) {
+    console.log(year);
+    for (var month = 6; month < 9; month++){
+        console.log(".........", month)
+    }
+
+}
+*/
+for (var i = 1; i <= 2; i++){
+    for (var j = 1; j <= 5; j++){
+        console.log("week" + i + "day" + j)
+    }
+}
